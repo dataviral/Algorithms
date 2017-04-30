5696fea2c7c5c656b6facafdbd973956738f0a2b
aviral <the1aviral@gmail.com>
2017-04-30 21:14:55 +0530

    Distribution Counting Sort

--- Distribution Counting Sort/distibutioncountingsort.py
+++ Distribution Counting Sort/distibutioncountingsort.py
@@ -1,16 +1,20 @@
-nums = [8,4,2,5,8,4,2,0,1]
+nums = [8,4,2,5,8,4,2,6,1,0]
 
 def countSort(nums):
-    d = [0] * max(nums)
-    s = [0] * nums
-    for i in range(len(nums)):
-        d[nums[i] ] += 1
-    for i in range(len(d)) :
-        d[j] = d[j-1] + d[j]
-    for i in reversed(range(0, len(nums))):
+    m = max(nums)
+    l = len(nums)
+    d = [0] * (m+1)
+    s = [0] * l
+    for i in range(l):
+        d[ nums[i] ] += 1
+    for i in range(1, m+1) :
+        d[i] = d[i-1] + d[i]
+    for i in reversed(range(0, l)):
         j = nums[i]
         s[d[j]-1] = nums[i]
-        d[j] -=1
+        d[j] -= 1
     return s
 
-countSort(nums)
+s = countSort(nums)
+
+print(s)
