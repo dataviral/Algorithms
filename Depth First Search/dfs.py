0b9057d5dcfdf5036dce59af431c19c0a34debfb
aviral <the1aviral@gmail.com>
2017-05-04 10:49:30 +0530

    DFS Algorithm Coded

--- Depth First Search/dfs.py
+++ Depth First Search/dfs.py
@@ -14,3 +14,36 @@ for each vertex w in V adjacent to w do
     if w is marked with 0
         dfs(w)
 """
+COUNT = 0
+
+Vertices = {
+    "0" : 0,
+    "1" : 0,
+    "2" : 0,
+    "3" : 0
+}
+
+adj_mat = {
+    "0" : [0, 1, 0, 0],
+    "1" : [1, 0, 1, 1],
+    "2" : [0, 1, 0, 0],
+    "3" : [0, 1, 0, 0]
+}
+
+def DFS(start_vetex):
+    dfs(start_vetex)
+    for v in Vertices:
+        if Vertices[v] == 0:
+            dfs(v)
+
+def dfs(v):
+    # print()
+    global COUNT
+    COUNT +=1
+    Vertices[v] = COUNT
+    for w in Vertices:
+        if Vertices[w] == 0 and adj_mat[v][int(w)] :
+            dfs(w)
+
+DFS("0")
+print(Vertices)
